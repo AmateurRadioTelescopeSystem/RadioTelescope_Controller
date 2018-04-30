841127aa91b75bbb0de07a3548dce9bd6ac5e7df
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-04-30 12:25:11 +0000

    Really minor changes made

--- RadioTele_PC.py
+++ RadioTele_PC.py
@@ -1,3 +1,4 @@
+from functools import partial
 import UInterface as UI
 import configData
 import logData
@@ -5,20 +6,14 @@ import TCPClient
 import sys
 
 if __name__ == '__main__':
+    logdata = logData.logData(__name__)  # Create the logger for the program
+
     app = UI.QtWidgets.QApplication(sys.argv)  # Create a Qt application instance
     RadioTelescopeControl = UI.QtWidgets.QMainWindow()  # Create the main window of th GUI
 
     # Create the contents of the windows
     ui = UI.Ui_RadioTelescopeControl()
     ui.setupUi(RadioTelescopeControl)
-
-    # Exception handling section for the log file code
-    try:
-        logdata = logData.logData(__name__)
-    except:
-        print(
-            "There is a problem with the handling of the log file. See log file for the traceback of the exception.\n")
-        exit(1)  # Terminate the script
 
     # Exception handling code for the XML file process
     try:
@@ -40,15 +35,13 @@ if __name__ == '__main__':
     s_alt = cfgData.getAltitude()  # Get the altitude from the settings file
     hostport = [cfgData.getHost(), cfgData.getPort()]  # Get the saved host address or name and port of the server
 
-
     # Give functionality to the buttons and add the necessary texts to fields
-    ui.connectRadioTBtn.clicked.connect(tcpClient.connectButton)
-    tcpClient.sendRequest("There")
+    ui.connectRadioTBtn.clicked.connect(partial(tcpClient.connectButton, actualPress = True))
     ui.lonTextInd.setText("<html><head/><body><p align=\"center\">%s<span style=\" "
-                          "vertical-align:super;\">o</span></p></body></html>" %s_latlon[1])
+                          "vertical-align:super;\">o</span></p></body></html>" % s_latlon[1])
     ui.latTextInd.setText("<html><head/><body><p align=\"center\">%s<span style=\" "
-                          "vertical-align:super;\">o</span></p></body></html>" %s_latlon[0])
-    ui.altTextInd.setText("<html><head/><body><p align=\"center\">%sm</p></body></html>" %s_alt)
+                          "vertical-align:super;\">o</span></p></body></html>" % s_latlon[0])
+    ui.altTextInd.setText("<html><head/><body><p align=\"center\">%sm</p></body></html>" % s_alt)
 
     RadioTelescopeControl.show()
     sys.exit(app.exec_())
