eb6561b92342c83998b21ac1b1804cac2bf5753a
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-05-02 14:34:44 +0000

    A handler for the TCP client thread

---
+++ ClientThread.py
@@ -0,0 +1,35 @@
+from PyQt5 import QtCore
+import TCPClient
+import time
+
+
+class ClientThread(QtCore.QThread):
+    # Create the signals to be used for data handling
+    conStatSig = QtCore.pyqtSignal(str, name='conClientStat')  # Connection indication signal
+    #dataRcvSigC = QtCore.pyqtSignal(str, name='dataClientRX')  # Send the received data out
+
+    def __init__(self, cfgData, ui, parent = None):
+        super(ClientThread, self).__init__(parent)  # Get the parent of the class
+        self.tcp = TCPClient.TCPClient(cfgData, ui)  # TCP handling object
+
+    def run(self):
+        if not self.tcp.sock_exst:
+            # Indicate that we are waiting for a connection once we start the program
+            self.conStatSig.emit("Connecting")  # Send the signal to indicate that we are waiting for connection
+            self.tcp.connect()  # Try to connect to the server
+
+            if self.tcp.sock_connected:
+                # If we have a connection, then we proceed and indicate that to the user
+                self.conStatSig.emit("Connected")  # Send the signal to indicate connection
+        elif not self.tcp.stopExec and self.tcp.sock_exst and self.tcp.threadSendReq and self.tcp.sock_connected:
+            recData = self.tcp.sendRequest(self.tcp.threadData)  # Send the request to the server
+            self.tcp.threadSendReq = False  # Reset the data reception flag
+
+            # If we receive zero length data, then that means the connection is broken
+            if len(recData) != 0:
+                recData = recData
+                #self.dataRcvSig.emit(recData)  # Send the data to be shown on the GUI widget
+        elif self.tcp.stopExec:
+            self.tcp.stopExec = False  # Reset the stopping flag
+            self.tcp.closeConnection()  # Close all sockets since client is gone
+            self.conStatSig.emit("Disconnected")  # Send the signal to indicate disconnectionS
