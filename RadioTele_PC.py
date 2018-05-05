c81d6c47644b3ca2db71bedce15558f1d76608dd
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-05-05 06:57:11 +0000

    Added the finsished attributes to the threads and also added the thread closure before app exit

--- RadioTele_PC.py
+++ RadioTele_PC.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3.5
 
 from PyQt5 import QtWidgets, QtCore
-from functools import partial
 from GUI_Windows import UInterface
 from Stellarium import StellariumThread
 from Client import ClientThread
@@ -30,16 +29,32 @@ def main():
         logdata.log("EXCEPT", "There is a problem with the XML file handling. Program terminates.", __name__)
         exit(1)  # Terminate the script
 
+    # TCP Stellarium server initialization
+    tcpStellThread = QtCore.QThread()  # Create a thread for the Stellarium server
+    tcpStell = StellariumThread.StellThread(cfgData)  # Create the instance of the TCP client
+    # Connect the signals from the client
+    tcpStell.moveToThread(tcpStellThread)  # Move the Stellarium server to a thread
+    tcpStell.conStatSigS.connect(ui.stellTCPGUIHandle)  # Connect the signal to the corresponding function
+    tcpStell.dataShowSigS.connect(ui.stellDataShow)  # Connect the signal to the corresponding function
+    # Connect the thread action signals
+    tcpStellThread.started.connect(tcpStell.start)  # Run the start code for the thread, once it starts
+    tcpStellThread.finished.connect(tcpStell.close)  # Run the stop code when a quit is requested
+    tcpStellThread.finished.connect(tcpStell.deleteLater)
+    tcpStellThread.finished.connect(tcpStellThread.deleteLater)
+
+    # RPi communications
     # TCP server for the RPi initialization
     tcpServerThread = QtCore.QThread()  # Create a thread for the TCP client
     tcpServer = RPiServerThread.RPiServerThread(cfgData)  # Create the instance of the TCP client
     # Connect the signals from the client
     tcpServer.moveToThread(tcpServerThread)  # Move the server to a thread
     tcpServer.conStatSigR.connect(ui.rpiTCPGUIHandle)  # Connection status signal for the server
-    #tcpServer.dataRcvSigC.connect(ui.signalTestrt)  # Received data signal
+    # tcpServer.dataRcvSigC.connect(ui.signalTestrt)  # Received data signal
     # Connect the thread action signals
     tcpServerThread.started.connect(tcpServer.start)  # What to do upon thread start
     tcpServerThread.finished.connect(tcpServer.close)  # What to do upon thread exit
+    tcpServerThread.finished.connect(tcpServer.deleteLater)
+    tcpServerThread.finished.connect(tcpServerThread.deleteLater)  # Wait until the run returns from the thread
 
     # Initialize the TCP client thread
     tcpClientThread = QtCore.QThread()  # Create a thread for the TCP client
@@ -50,35 +65,29 @@ def main():
     # Connect the thread action signals
     tcpClientThread.started.connect(tcpClient.start)  # Connect with this function upon thread start
     tcpClientThread.finished.connect(tcpClient.close)  # Connect with thsi function upon thread exit
-
-    # TCP Stellarium server initialization
-    tcpStellThread = QtCore.QThread()  # Create a thread for the Stellarium server
-    tcpStell = StellariumThread.StellThread(cfgData)  # Create the instance of the TCP client
-    # Connect the signals from the client
-    tcpStell.moveToThread(tcpStellThread)  # Move the Stellarium server to a thread
-    tcpStell.conStatSigS.connect(ui.stellTCPGUIHandle)  # Connect the signal to the corresponding function
-    tcpStell.dataShowSigS.connect(ui.stellDataShow)  # Connect the signal to the corresponding function
-    #tcpStell.sendClientConn.connect(tcpClient.send)  # Send the appropriate command request to the RPi server
-    # Connect the thread action signals
-    tcpStellThread.started.connect(tcpStell.start)  # Run the start code for the thread, once it starts
-    tcpStellThread.finished.connect(tcpStell.close)  # Run the stop code when a quit is requested
+    tcpClientThread.finished.connect(tcpClient.deleteLater)
+    tcpClientThread.finished.connect(tcpClientThread.deleteLater)  # Wait until the run returns from the thread
 
     # Initialize the operation handler
     operHandlerThread = QtCore.QThread()  # Create a thread for the operation handler
-    operHandle = OperationHandler.OpHandler(tcpClient, tcpServer, tcpStell, ui)
+    operHandle = OperationHandler.OpHandler(tcpClient, tcpServer, tcpStell,
+                                            tcpClientThread, tcpServerThread, tcpStellThread, ui)
     operHandle.moveToThread(operHandlerThread)  # Move the operation handler to a thread
     operHandlerThread.started.connect(operHandle.start)  # Run the start method upon thread start
+    operHandlerThread.finished.connect(operHandle.appExitRequest)  # If the handler exits, then the app cloes
+    operHandlerThread.finished.connect(operHandle.deleteLater)  # Wait until the run returns from the thread
+    operHandlerThread.finished.connect(operHandlerThread.deleteLater)
     operHandlerThread.start()  # Start the operation handler
 
     s_latlon = cfgData.getLatLon()  # First element is latitude and second element is longitude
     s_alt = cfgData.getAltitude()  # Get the altitude from the settings file
     autoconStell = cfgData.getTCPStellAutoConnStatus()  # See if auto-connection at startup is enabled
-    autoconRPi = cfgData.getTCPAutoConnStatus()  # Get the auto-connection preference for the RPi
+    autoconRPi = cfgData.getTCPAutoConnStatus()  # Get the auto-connection preference for the RPi server and client
 
-    # Give functionality to the buttons and add the necessary texts to fields
-    ui.connectRadioTBtn.clicked.connect(partial(operHandle.connectButtonR, thread=tcpClientThread))
-    ui.serverRPiConnBtn.clicked.connect(partial(operHandle.connectButtonRPi, thread=tcpServerThread))
-    ui.connectStellariumBtn.clicked.connect(partial(operHandle.connectButtonS, thread=tcpStellThread))
+    # Give functionality to the buttons
+    ui.connectRadioTBtn.clicked.connect(operHandle.connectButtonR)  # TCP client connection button
+    ui.serverRPiConnBtn.clicked.connect(operHandle.connectButtonRPi)  # TCP server connection button
+    ui.connectStellariumBtn.clicked.connect(operHandle.connectButtonS)  # Stellarium TCP server connection button
 
     # If auto-connection is selected for thr TCP section, then do as requested
     if autoconStell == "yes":
@@ -94,7 +103,8 @@ def main():
                           "vertical-align:super;\">o</span></p></body></html>" % s_latlon[0])
     ui.altTextInd.setText("<html><head/><body><p align=\"center\">%sm</p></body></html>" % s_alt)
 
-    #tcpStell.sendClientConn.emit(9.29947, -16.29769)
+    # We quit from the operation handle thread and then we exit. All handling is done there
+    app.aboutToQuit.connect(operHandlerThread.quit)
 
     # Start the application
     ui.show_application()  # Render and show the GUI main window
