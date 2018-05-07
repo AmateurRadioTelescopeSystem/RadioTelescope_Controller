20a6b2840df8c257e46068f97249bebbe64ffe71
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-05-07 19:30:55 +0000

    Relocated the signals

--- OperationHandler.py
+++ OperationHandler.py
@@ -1,8 +1,11 @@
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore
+from functools import partial
 import logData
 
 
 class OpHandler(QtCore.QObject):
+    posDataShow = QtCore.pyqtSignal(float, float, name='posDataShow')
+
     def __init__(self, tcpClient, tcpServer, tcpStellarium, tcpClThread, tcpServThread, tcpStelThread, ui, parent=None):
         super(OpHandler, self).__init__(parent)
         self.tcpClient = tcpClient  # TCP client object
@@ -18,12 +21,11 @@ class OpHandler(QtCore.QObject):
         self.logD = logData.logData(__name__)  # Data logger object
 
     def start(self):
-        print("Ops handle thread: %d" % int(QtCore.QThread.currentThreadId()))  # Used in debugging
-        self.tcpStellarium.sendClientConn.connect(self.stellCommSend)  # Send data from Stellarium to the RPi
-        self.tcpClient.dataRcvSigC.connect(self.clientDataRx)  # Receive pending data from RPi connected client
-        self.tcpServer.dataRxFromServ.connect(self.rpiServRcvData)  # Receive data from the RPi server
-        self.tcpServer.clientNotice.connect(self.tcpClient.connect)  # Tell the client to reconnect
-        self.ui.stopMovingRTSig.connect(self.stopMovingRT)  # Send a motion stop command, once this signal is triggered
+        print("Operations handle thread ID: %d" % int(QtCore.QThread.currentThreadId()))  # Used in debugging
+        self.prev_pos = ["", ""]  # The dish position is saved for change comparison
+        self.signalConnectios()  # Make all the necessary signal connections
+
+        self.val = 0.0
 
     # Client connection button handling method
     def connectButtonR(self):
@@ -63,7 +65,7 @@ class OpHandler(QtCore.QObject):
     # Dta received from the client connected to the RPi server
     @QtCore.pyqtSlot(str, name='dataClientRX')
     def clientDataRx(self, data: str):
-        print("Data received from client: %s" % data)
+        print("Data received from client (RPi server connected): %s" % data)
 
     # Send the appropriate command according to the selected mode. Data is received from Stellarium (sendClientConn)
     @QtCore.pyqtSlot(list, name='clientCommandSendStell')
@@ -79,17 +81,31 @@ class OpHandler(QtCore.QObject):
     @QtCore.pyqtSlot(str, name='rpiServDataRx')
     def rpiServRcvData(self, data: str):
         spltData = data.split("_")  # Split the string with the set delimiter
-        if spltData[0] == "DISHPOS":
-            ra = float(spltData[2])  # Get the RA from the received position
-            dec = float(spltData[4])  # Get the DEC from the received position
-            self.ui.raPosInd.setText("%.5fh" % ra)  # Show the RA of the dish on the GUI
-            self.ui.decPosInd.setText("%.5f" % dec + u"\u00b0")  # Show the declination of the dish on the GUI
-            self.tcpStellarium.sendDataStell.emit(ra, dec)  # Send the data to Stellarium
+        if spltData[0] == "DISHPOS" or spltData[0] == "POSUPDATE":
+            ra = spltData[2]  # Get the RA from the received position
+            dec = spltData[4]  # Get the DEC from the received position
+            if not (self.prev_pos[0] == ra and self.prev_pos[1] == dec and spltData[0] != "POSUPDATE"):
+                self.tcpStellarium.sendDataStell.emit(float(ra), float(dec))  # Send the position to Stellarium
+                self.posDataShow.emit(float(ra), float(dec))  # Send the updated values if they are different
+            self.prev_pos = [ra, dec]  # Save the values for later comparison
 
     # Command to stop any motion of the radio telescope dish
     @QtCore.pyqtSlot(name='stopRadioTele')
     def stopMovingRT(self):
+        # TODO change the command to the appropriate one
         self.tcpClient.sendData.emit("STOP")  # Send the request to stop moving to the RPi server
+
+    # Make all the necessary signal connections
+    def signalConnectios(self):
+        self.tcpStellarium.sendClientConn.connect(self.stellCommSend)  # Send data from Stellarium to the RPi
+        self.tcpClient.dataRcvSigC.connect(self.clientDataRx)  # Receive pending data from RPi connected client
+
+        self.tcpServer.dataRxFromServ.connect(self.rpiServRcvData)  # Receive data from the RPi server
+        self.tcpServer.clientNotice.connect(self.tcpClient.connect)  # Tell the client to reconnect
+
+        self.ui.stopMovingRTSig.connect(self.stopMovingRT)  # Send a motion stop command, once this signal is triggered
+        self.ui.stellPosUpdtBtn.clicked.connect(partial(self.tcpClient.sendData.emit, "SEND_POS_UPDATE"))
+        self.posDataShow.connect(self.ui.posDataShow)  # Show the dish position data, when available
 
     # This function is called whenever the app is about to quit
     # First quit from the thread and then delete both the thread and the corresponding object
