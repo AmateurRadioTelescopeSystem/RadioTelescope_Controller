1319c2a8d0c103eb4a33e1b63735ceb8e17e543d
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-10-08 17:04:53 +0000

    Corrected a small typing erros

--- build_test.sh
+++ build_test.sh
@@ -2,6 +2,6 @@
 
 set -e  # Stop if any command fails
 
-chmod +x Tests/./program_tester.py  # Change execution permissions for the file
+chmod +x Tests/program_tester.py  # Change execution permissions for the file
 pyrcc5 Icons/GUI_Images/Image_Resources.qrc -o Core/GUI/resources.py  # Create the resources file
 Tests/./program_tester.py  # Run the main program
