2b5dc515b6bae87c984241433742c93888fecd81
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-10-08 17:05:51 +0000

    Added a main program runner script
    Main radio telescope program runs from this file.
    Any executable permisions are covered in the script.

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e  # Stop if any command fails
+
+chmod +x Core/RadioTele_PC.py  # Change execution permissions for the file
+Core/.RadioTele_PC.py  # Execute the main program file
