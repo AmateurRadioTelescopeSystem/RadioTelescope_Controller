fce3126b911f73186e0a954f28ac91f7c74e107c
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-10-09 06:51:33 +0000

    Corrected a typo

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 set -e  # Stop if any command fails
 
 chmod +x Core/RadioTele_PC.py  # Change execution permissions for the file
-Core/.RadioTele_PC.py  # Execute the main program file
+Core/./RadioTele_PC.py  # Execute the main program file
