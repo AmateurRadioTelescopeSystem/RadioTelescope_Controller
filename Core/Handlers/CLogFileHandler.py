a85f8fc25f9249f54240235a883eba574b10299f
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-10-18 19:05:30 +0000

    Major changes to the log handler
    - Removed the multiprocessing feature, because it was not needed
    - Exception is now propagating through to the end file
    - Added a second custom rotating file handler, which manages the size of the debugging info file

--- Core/Handlers/CLogFileHandler.py
+++ Core/Handlers/CLogFileHandler.py
@@ -14,12 +14,9 @@ import logging.handlers
 # Code for compression, retrieved from logging docs:
 # https://docs.python.org/3/howto/logging-cookbook.html#using-a-rotator-and-namer-to-customize-log-rotation-processing
 # ============================================================================
-class CustomLogHandler(logging.Handler):
-    """multiprocessing log handler
-
-    This handler makes it possible for several processes
-    to log to the same file by using a queue.
-
+class CustomLogTimedRotationHandler(logging.Handler):
+    """
+    Customize the timed rotating file handler to include old log compression
     """
     def __init__(self, filename, when, backup_count, enc, utc):
         """
@@ -32,17 +29,10 @@ class CustomLogHandler(logging.Handler):
         """
         logging.Handler.__init__(self)
 
-        self.queue = multiprocessing.Queue(-1)
-        self.fname = filename
-        self.when = when
-        self.encoding = enc
-        self.utc = utc
-        self.backup_count = backup_count
-        self._handler = None
-
-        thrd = threading.Thread(target=self.receive)
-        thrd.daemon = True
-        thrd.start()
+        self._handler = logging.handlers.TimedRotatingFileHandler(
+            filename, when, backupCount=backup_count, encoding=enc, utc=utc)
+        self._handler.rotator = self.compressor  # Compress the old file in every rotation
+        self._handler.namer = self.namer  # Name the compressed file
 
     def setFormatter(self, fmt):
         """
@@ -50,52 +40,91 @@ class CustomLogHandler(logging.Handler):
         :param fmt: Formatter string as passed from the configuration
         :return: Nothing
         """
-        self.fmt = fmt  # Save the format to use it for the handler later
-
-    def receive(self):
-        """
-        Receive the logging message an perform logging.
-        :return: Nothing
-        """
-        self._handler = logging.handlers.TimedRotatingFileHandler\
-            (self.fname, self.when, backupCount=self.backup_count, encoding=self.encoding, utc=self.utc)
-        logging.Handler.setFormatter(self, self.fmt)
-        self._handler.setFormatter(self.fmt)
-        self._handler.rotator = self.compressor  # Compress the old file in every rotation
-        self._handler.namer = self.namer  # Name the compressed file
-        while True:
-            try:
-                record = self.queue.get()  # Get the data from the queue
-                if record.args:
-                    record.msg = record.msg % record.args
-                    record.args = None
-                if record.exc_info:
-                    dummy = self.format(record)
-                    record.exc_info = None
-                self._handler.emit(record)  # Write to log file
-            except (KeyboardInterrupt, SystemExit):
-                raise
-            except EOFError:
-                break
-            except:
-                traceback.print_exc(file=sys.stderr)
-
-    def send(self, s):
-        """
-        Send the log message to the logging queue, in order to be logged.
-        :param s: Message object
-        :return: Nothing
-        """
-        self.queue.put_nowait(s)
+        logging.Handler.setFormatter(self, fmt)
+        self._handler.setFormatter(fmt)
 
     def emit(self, record):
         """
-        Emit the log recording signal by sending the message to the queue
+        Emit the message to the new handler
         :param record: Message record
         :return: Nothing
         """
         try:
-            self.send(record)
+            self._handler.emit(record)  # Write to log file
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+
+    def close(self):
+        """
+        Close the handler upon request.
+        :return: Nothing
+        """
+        if self._handler is not None:
+            self._handler.close()
+        logging.Handler.close(self)
+
+    def compressor(self, source, dest):
+        """
+        Compress the file on every rotation
+        :param source: Source file name
+        :param dest: Destination file name
+        :return: Nothing
+        """
+        with open(source, "rb") as source_file:
+            data = source_file.read()  # Read the data from file and compress them
+            compressed = gzip.compress(data, 9)  # Get the compressed binary data
+            with open(dest, "wb") as dest_file:
+                dest_file.write(compressed)  # Save the compressed data to file
+        os.remove(source)  # Remove uncompressed file
+
+    def namer(self, name):
+        """
+        Append the appropriate suffix to the compressed file name
+        :param name: File name to append suffix
+        :return: Nothing
+        """
+        return name + ".gz"
+
+
+class CustomLogRotationHandler(logging.Handler):
+    """
+    Customize the rotating file handler to include old log compression and size rotation
+    """
+    def __init__(self, filename, max_bytes, backup_count, enc, delay=False):
+        """
+        Class constructor to make the necessary initializations.
+        :param filename: The name of the logging file
+        :param when: When to rotate
+        :param backup_count: Max number of old files to keep
+        :param enc: Log file's encoding
+        :param utc: UTC to be used as logging time
+        """
+        logging.Handler.__init__(self)
+
+        self._handler = logging.handlers.RotatingFileHandler(
+            filename, maxBytes=max_bytes, backupCount=backup_count, encoding=enc, delay=delay)
+        self._handler.rotator = self.compressor  # Compress the old file in every rotation
+        self._handler.namer = self.namer  # Name the compressed file
+
+    def setFormatter(self, fmt):
+        """
+        Set the formatter for the file handler
+        :param fmt: Formatter string as passed from the configuration
+        :return: Nothing
+        """
+        logging.Handler.setFormatter(self, fmt)
+        self._handler.setFormatter(fmt)
+
+    def emit(self, record):
+        """
+        Emit the message to the new handler
+        :param record: Message record
+        :return: Nothing
+        """
+        try:
+            self._handler.emit(record)  # Write to log file
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
