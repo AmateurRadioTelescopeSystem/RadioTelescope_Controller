a93c04d9b1440b3910b8bd3b5fd6631263ce4b5a
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-10-19 05:28:39 +0000

    Include a configuration file to setup the program

---
+++ configure.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e  # Stop if any command fails
+
+chmod +x run.sh  # Change execution permissions for the file
+sudo pip3 install -r requirements.txt  # Install the necessary python packages
+
+echo "Creating the resources file....."
+pyrcc5 Icons/GUI_Images/Image_Resources.qrc -o Core/GUI/resources.py  # Create the resources file
+echo "Configuration done!! You are all set and you can use the main program!!"  # Show a success message
