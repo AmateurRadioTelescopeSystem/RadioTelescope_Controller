b0d89c76a854a5ac568c92b4bfd1e4abc2d70627
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-10-19 15:48:30 +0000

    Removed unnecessary line

--- build_test.sh
+++ build_test.sh
@@ -3,5 +3,4 @@
 set -e  # Stop if any command fails
 
 chmod +x Tests/program_tester.py  # Change execution permissions for the file
-pyrcc5 Icons/GUI_Images/Image_Resources.qrc -o Core/GUI/resources.py  # Create the resources file
 Tests/./program_tester.py  # Run the main program
