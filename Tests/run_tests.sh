8318898ac2c21ba54b329e4f3b6d48a4e08d03e3
Dimitrios Stoupis <dimitris.apple@gmail.com>
2019-03-27 18:01:21 +0000

    Run the tests in the correct order

--- Tests/run_tests.sh
+++ Tests/run_tests.sh
@@ -3,4 +3,5 @@
 set -e  # Stop when a command fails
 
 # Temporary, will be  automatic once tests are complete
+python3 -m unittest Tests/test_integrated.py
 find Tests/ -type f -iname "test_*.py" | xargs python3 -m unittest
--- build_test.sh
+++ build_test.sh
@@ -2,5 +2,4 @@
 
 set -e  # Stop if any command fails
 
-chmod +x Tests/program_tester.py  # Change execution permissions for the file
-Tests/IntegratedRun.py  # Run the main program
+./Tests/run_tests.sh  # Run the main program
