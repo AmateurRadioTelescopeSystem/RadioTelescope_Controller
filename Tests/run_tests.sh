66b4d193d7a86a54e3563b16ebcadd29a6bf09cb
Dimitrios Stoupis <dimitris.apple@gmail.com>
2019-03-28 12:18:11 +0000

    Organize directories better

--- Tests/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e  # Stop when a command fails
-
-# Temporary, will be  automatic once tests are complete
-python3 Tests/RunTests.py
--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 set -e  # Stop if any command fails
-
-./Tests/run_tests.sh  # Run the main program
+./Tests/RunTests.py  # Run the tests
